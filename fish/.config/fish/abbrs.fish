f450df5c03679a1893194b6bb79c3fb68695eebd
soham <soham@posteo.net>
2020-03-23 17:00:49 -0500

    Deleted fish. Changed background.

--- fish/.config/fish/abbrs.fish
+++
@@ -1,3 +0,0 @@
-alias poweroff "loginctl poweroff"
-alias reboot "loginctl reboot"
-alias zzz "loginctl suspend"
--- fish/.config/fish/environment.fish
+++
@@ -1,9 +0,0 @@
-set -gx EDITOR emacs -nw
-set -gx PAGER less
-set -gx BROWSER qutebrowser
-
-# PATH
-set PATH ~/bin $PATH
-set PATH ~/.local/bin $PATH
-set PATH ~/miniconda3/bin $PATH
-set PATH ~/.local/texlive/2019/bin/x86_64-linux $PATH
